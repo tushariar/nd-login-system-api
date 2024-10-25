dc566e16050217dc621074fc793bbff9662a5467
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2024-10-25 19:30:47 +0600

    feat: added utility functions for authentication

---
+++ src/utilities/auth.ts
@@ -0,0 +1,20 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { v4 as uuidV4 } from "uuid";
+
+export const generateToken = (userId: string, public_key: string) => {
+  return jwt.sign({ id: userId, public_key }, process.env.JWT_SECRET!, {
+    expiresIn: "7d",
+  });
+};
+
+export const generateOTP = () => {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+};
+
+export const generateResetToken = () => {
+  const key = uuidV4().toUpperCase();
+  const hash = bcrypt.hashSync(key, 10);
+
+  return { key, hash };
+};
